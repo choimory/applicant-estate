99e7ca11af50ea4af3a77ca3f21c6b1c8f305a1d
choimory <choimory@naver.com>
2025-03-02 16:40:24 +0900

    Initial commit

---
+++ src/main/java/com/choimory/applicantestate/ApplicantEstateApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.applicantestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantEstateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicantEstateApplication.class, args);
+    }
+
+}
