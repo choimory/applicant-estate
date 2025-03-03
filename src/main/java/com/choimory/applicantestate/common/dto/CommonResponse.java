62eea81fbef6177880ab5e98cf0ac21e206269ca
choimory <choimory@naver.com>
2025-03-03 20:58:17 +0900

    feature: 기본 전역예외 작성

--- src/main/java/com/choimory/applicantestate/common/dto/CommonResponse.java
+++ src/main/java/com/choimory/applicantestate/common/dto/CommonResponse.java
@@ -1,4 +1,15 @@
 package com.choimory.applicantestate.common.dto;
 
-public class CommonResponse {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+@Getter
+public class CommonResponse<T> {
+    @Builder.Default
+    private final int status = 0;
+    private final String message;
+    private final T data;
 }
--- src/main/java/com/choimory/applicantestate/common/exception/ExceptionAdvice.java
+++ src/main/java/com/choimory/applicantestate/common/exception/ExceptionAdvice.java
@@ -1,4 +1,37 @@
 package com.choimory.applicantestate.common.exception;
 
+import com.choimory.applicantestate.common.dto.CommonResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 public class ExceptionAdvice {
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CommonResponse<String> exception(Exception e) {
+        return CommonResponse.<String>builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .data(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CommonResponse<String> runtimeException(Exception e) {
+        return CommonResponse.<String>builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .data(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler({CommonException.class})
+    public ResponseEntity<CommonResponse<String>> commonException(CommonException e) {
+        return new ResponseEntity<>(CommonResponse.<String>builder()
+                .status(e.getStatus())
+                .message(e.getMessage())
+                .build(), e.getHttpStatus());
+    }
 }
